e1f38c499400b16ac419cf8e9c18a94d22fa7fe4
devgenx <sebastian110420@gmail.com>
2023-05-04 02:06:40 -0700

    Login user testcases

---
+++ src/components/Login.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+const Login = () => {
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState({});
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users/1"
+      );
+
+      const data = await response.json();
+      setLoading(false);
+      setUser(data);
+    } catch (err) {
+      setError(true);
+    }
+  };
+
+  return (
+    <div className="container">
+      <span className="user">{user?.name}</span>
+      <form>
+        <input
+          type="text"
+          placeholder="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button disabled={!username || !password} onClick={handleClick}>
+          {loading ? "please wait" : "Login"}
+        </button>
+        <span
+          data-testid="error"
+          style={{ visibility: error ? "visible" : "hidden" }}
+        >
+          Something went wrong!
+        </span>
+      </form>
+    </div>
+  );
+};
+export default Login;
---
+++ src/components/Login.test.js
@@ -0,0 +1,96 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import Login from "./Login";
+
+test("username input should be rendered", () => {
+  render(<Login />);
+  const userInput = screen.getByPlaceholderText(/username/i);
+  expect(userInput).toBeInTheDocument();
+});
+
+test("password input should be rendered", () => {
+  render(<Login />);
+  const passwordInput = screen.getByPlaceholderText(/password/i);
+  expect(passwordInput).toBeInTheDocument();
+});
+
+test("button input should be rendered", () => {
+  render(<Login />);
+  const buttonEl = screen.getByRole("button");
+  expect(buttonEl).toBeInTheDocument();
+});
+
+test("username input should be empty", () => {
+  render(<Login />);
+  const userInput = screen.getByPlaceholderText(/username/i);
+  expect(userInput.value).toBe("");
+});
+
+test("password input should be empty", () => {
+  render(<Login />);
+  const passwordInput = screen.getByPlaceholderText(/password/i);
+  expect(passwordInput.value).toBe("");
+});
+
+test("button should be disabled", () => {
+  render(<Login />);
+  const buttonEl = screen.getByRole("button");
+  expect(buttonEl).toBeDisabled();
+});
+
+test("loading should not be rendered", () => {
+  render(<Login />);
+  const buttonEl = screen.getByRole("button");
+  expect(buttonEl).not.toHaveTextContent(/Please wait/i);
+});
+
+test("error shouldn't be visible", () => {
+  render(<Login />);
+  const error = screen.getByTestId("error");
+  expect(error).not.toBeVisible();
+});
+
+test("username input should change", () => {
+  render(<Login />);
+  const userInput = screen.getByPlaceholderText(/username/i);
+  const testValue = "testValue";
+
+  fireEvent.change(userInput, { target: { value: testValue } });
+  expect(userInput.value).toBe(testValue);
+});
+
+test("password input should change", () => {
+  render(<Login />);
+  const passwordInput = screen.getByPlaceholderText(/password/i);
+  const testPass = "testPass";
+  fireEvent.change(passwordInput, { target: { value: testPass } });
+
+  expect(passwordInput.value).toBe(testPass);
+});
+
+test("button shouldn't be disabled when inputs exist", () => {
+  render(<Login />);
+  const buttonEl = screen.getByRole("button");
+  const userInput = screen.getByPlaceholderText(/username/i);
+  const passwordInput = screen.getByPlaceholderText(/password/i);
+
+  const testValue = "test";
+
+  fireEvent.change(userInput, { target: { value: testValue } });
+  fireEvent.change(passwordInput, { target: { value: testValue } });
+
+  expect(buttonEl).not.toBeDisabled();
+});
+
+test("loading should be rendered when clicked", () => {
+  render(<Login />);
+  const buttonEl = screen.getByRole("button");
+  const userInput = screen.getByPlaceholderText(/username/i);
+  const passwordInput = screen.getByPlaceholderText(/password/i);
+
+  const testValue = "test";
+
+  fireEvent.change(userInput, { target: { value: testValue } });
+  fireEvent.change(passwordInput, { target: { value: testValue } });
+  fireEvent.click(buttonEl);
+  expect(buttonEl).toHaveTextContent(/please wait/i);
+});
